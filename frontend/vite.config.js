d02aacdd47ec3405f92131c8946140720423096a
sachinmandloi06 <sachinmandloi06@gmail.com>
2024-12-22 22:52:42 +0530

    change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'https://crud-backend-qjhm.onrender.com',
-  //       changeOrigin: true,
-  //       secure: false,
-  //     }
-  //   }
-  // }
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://crud-backend-qjhm.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
