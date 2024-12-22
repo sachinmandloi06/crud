2a56c29ceb14bed92782d0b5c41619e0a6f1cc33
sachinmandloi06 <sachinmandloi06@gmail.com>
2024-12-22 15:55:30 +0530

    first

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://crud-sivb.onrender.com',
         changeOrigin: true,
         secure: false,
       }
