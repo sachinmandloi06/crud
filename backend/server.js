0079e15caafac7e29aec30cedee8018ca28bf70d
sachinmandloi06 <sachinmandloi06@gmail.com>
2024-12-22 17:43:42 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,9 @@
 const express = require("express")
 const colors = require("colors")
 const connectDB = require("./config/db_config")
-const path = require("path")
 require('dotenv').config()
 
 const app = express()
-const _dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000
 
@@ -26,10 +24,6 @@ app.get("/" ,(req , res) => {
 // Todo Routes
 app.use('/api/todo' , require('./routes/todoRoutes'))
 
-app.use(express.static(path.join(_dirname,"/frontend/dist")))
-app.get('*',(req,res) => {
-    res.sendFile(path.resolve(_dirname, "frontend","dist","index.html"))
-})
 
 app.listen(PORT , () => {
     console.log(`SERVER IS RUNNING AT PORT : ${PORT}`.bgBlue.white)
